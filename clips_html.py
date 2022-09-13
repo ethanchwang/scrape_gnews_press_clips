9e44b139c05848fa50e9364305b32afc564a31cd
ethanchwang <ethanhwang2@gmail.com>
2022-09-13 15:43:07 -0400

    output html

--- clips_html.py
+++ clips_html.py
@@ -1,11 +1,10 @@
 from gnews import GNews
 from newspaper import Article
-from docx import Document
-import docx
 import datetime
 from deep_translator import GoogleTranslator
 from dataclasses import dataclass,field
 from typing import List
+from bs4 import BeautifulSoup
 
 @dataclass
 class mention_article:
@@ -31,48 +30,9 @@ def find_lead(paragraph_list):
             del paragraph_list[idx]
             return paragraph
 
-def add_hyperlink(paragraph, url, text, color, underline):
-    # This gets access to the document.xml.rels file and gets a new relation id value
-    part = paragraph.part
-    r_id = part.relate_to(url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True)
-
-    # Create the w:hyperlink tag and add needed values
-    hyperlink = docx.oxml.shared.OxmlElement('w:hyperlink')
-    hyperlink.set(docx.oxml.shared.qn('r:id'), r_id, )
-
-    # Create a w:r element
-    new_run = docx.oxml.shared.OxmlElement('w:r')
-
-    # Create a new w:rPr element
-    rPr = docx.oxml.shared.OxmlElement('w:rPr')
-
-    # Add color if it is given
-    if not color is None:
-        c = docx.oxml.shared.OxmlElement('w:color')
-        c.set(docx.oxml.shared.qn('w:val'), color)
-        rPr.append(c)
-
-    # Remove underlining if it is requested
-    if not underline:
-        u = docx.oxml.shared.OxmlElement('w:u')
-        u.set(docx.oxml.shared.qn('w:val'), 'none')
-        rPr.append(u)
-
-    # Join all the xml elements together add add the required text to the w:r element
-    new_run.append(rPr)
-    new_run.text = text
-    new_run.bold = True
-    new_run.name = 'Arial'
-    hyperlink.append(new_run)
-
-    paragraph._p.append(hyperlink)
-
-    return hyperlink
-
 def query_mentions(query:str, time_frame:str, language:str) -> list:
     all_mentions_articles = []
 
-    #instantiate GNews object
     google_news = GNews(period=time_frame,max_results=10)
     google_news.language = language
     query_results = [] + google_news.get_news(query)
@@ -158,27 +118,21 @@ def query_topics(topics:list,geographical_area:str) -> list:
     return all_topic_articles
 
 #creates paragraph that bolds name of query
-def bold_name(document,name:str,text:str):
+def bold_name(name:str,text:str):
+    temp_html = '<h1>'
+
     delimiter = name.split()[-1].title()
     split_paragraph = text.split(delimiter)
 
-    p = add_paragraph_without_spacing(document=document)
     for index,text in enumerate(split_paragraph):
-        p.add_run(text,style='paragraph_text')
+        temp_html += text
         if index != len(split_paragraph)-1:
-            p.add_run(delimiter,style='bold_text')
-
-def add_paragraph_without_spacing(document,text='',style=None):
-    if style==None:
-        p = document.add_paragraph(text)
-    else:
-        p = document.add_paragraph(text,style)
-    p.paragraph_format.space_before = docx.shared.Pt(0)
-    p.paragraph_format.space_after = docx.shared.Pt(0)
-    return p
+            temp_html += f'''<b>{delimiter}</b>'''
+    
+    return temp_html + '</h1>'
 
 def main():
-    document = Document()
+    html = '''<head><link rel="stylesheet" href="style.css"></head>'''
 
     #get name of person to query
     query = "adriano espaillat"
@@ -187,68 +141,44 @@ def main():
     time_frame = "24h"
     topics_to_query = ['Housing','Education','Labor','Budget','Immigration','Guns']
 
-    #instantiate font styles
-    style = document.styles
-    title_style = style.add_style('title', docx.enum.style.WD_STYLE_TYPE.PARAGRAPH)
-    font = title_style.font
-    font.size = docx.shared.Pt(11)
-    font.name = 'Arial'
-    font.bold = True
-    font.underline = True
-
-    paragraph_style = style.add_style('paragraph_text', docx.enum.style.WD_STYLE_TYPE.CHARACTER)
-    paragraph_font = paragraph_style.font
-    paragraph_font.size = docx.shared.Pt(11)
-    paragraph_font.name = 'Arial'
-
-    bold_style = style.add_style('bold_text', docx.enum.style.WD_STYLE_TYPE.CHARACTER)
-    bold_font = bold_style.font
-    bold_font.size = docx.shared.Pt(11)
-    bold_font.name = 'Arial'
-    bold_font.bold = True
-
     #title
-    p = add_paragraph_without_spacing(document=document,text=f"""CONGRESSMAN {query.split(" ")[1].upper()} MENTIONS:""",style='title')
+    html += f'<h1><b><u>CONGRESSMAN {query.split(" ")[1].upper()} MENTIONS:</b></u></h1><br>'
 
     #english clips
     for article in query_mentions(query=query,time_frame=time_frame,language='english'):
-        p = document.add_paragraph("",style='title')
-        p.paragraph_format.space_before = docx.shared.Pt(0)
-        p.paragraph_format.space_after = docx.shared.Pt(0)
-        add_hyperlink(p, article.url, article.title+f" -- {article.publisher} {article.month_num}/{article.day_num}/22", '0000FF', True)
-        p = add_paragraph_without_spacing(document=document)
-        p.add_run(article.lead_text,style='paragraph_text')
-        p = add_paragraph_without_spacing(document=document)
+        
+        html += f'''<h1><b><u><a href="{article.url}" target="_blank">{article.title} -- {article.publisher} {article.month_num}/{article.day_num}/22</a></u></b></h1>'''
+        html += f'''<h1>{article.lead_text}</h1><br>'''
         for paragraph in article.mention_text:
-            bold_name(document=document,name=query,text=paragraph)
-            p = add_paragraph_without_spacing(document=document)
+            html += bold_name(name=query,text=paragraph) + '<br>'
 
     #spanish clips
     for article in query_mentions(query=query,time_frame=time_frame,language='spanish'):
-        p = document.add_paragraph("",style='title')
-        p.paragraph_format.space_before = docx.shared.Pt(0)
-        p.paragraph_format.space_after = docx.shared.Pt(0)
-        add_hyperlink(p, article.url, article.title+f" -- {article.publisher} {article.month_num}/{article.day_num}/22", '0000FF', True)
-        p = add_paragraph_without_spacing(document=document)
-        p.add_run(article.lead_text,style='paragraph_text')
+        html += f'''<h1><b><u><a href="{article.url}" target="_blank">{article.title} -- {article.publisher} {article.month_num}/{article.day_num}/22</a></u></b></h1>'''
+        html += f'''<h1>{article.lead_text}</h1><br>'''
         for paragraph in article.mention_text:
-            bold_name(document=document,name=query,text=paragraph)
-            p = add_paragraph_without_spacing(document=document)
+            html += bold_name(name=query,text=paragraph) + '<br>'
 
     #topic clips
     topic_query_results = query_topics(topics=topics_to_query,geographical_area=area)
-    p = add_paragraph_without_spacing(document=document,text="PRESS CLIPS:",style='title')
+    html += '<h1><b><u>PRESS CLIPS:</b></u></h1>'
     for topic in topics_to_query:
-        p = add_paragraph_without_spacing(document=document)
-        p.add_run("-",style='paragraph_text')
-        p = add_paragraph_without_spacing(document=document,text=f"{topic.title()}:",style='title')
+        html += f'''<h1>-</h1>'''
+        html += f'<h1><b><u{topic.title()}</b></u></h1>'
         for article in topic_query_results[topic]:
-            p = document.add_paragraph("",style='title')
-            p.paragraph_format.space_before = docx.shared.Pt(0)
-            p.paragraph_format.space_after = docx.shared.Pt(0)
-            add_hyperlink(p, article.url, article.title+f" -- {article.publisher} {article.month_num}/{article.day_num}/22", '0000FF', True)
+            html += f'''<h1><b><u><a href="{article.url}" target="_blank">{article.title} -- {article.publisher} {article.month_num}/{article.day_num}/22</a></u></b></h1>'''
 
-    document.save(f'Press Clips {datetime.date.today()}.docx')
+    path = '/Users/ethanhwang/Documents/___projects/scrape_gnews_press_clips/'
+    html = html.replace('’',"""'""")
+    html = html.replace('–','-')
+    html = html.replace('“','"')
+    html = html.replace('‘',"""'""")
+    html = html.replace('”','"')
+    html = html.replace('—','-')
+
+    soup = BeautifulSoup(html, 'html.parser')
+    with open(path + f'Press Clips {datetime.date.today()}.html','w') as file:
+        file.write(soup.prettify())
 
 if __name__ == '__main__':
     main()
